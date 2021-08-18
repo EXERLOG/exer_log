e17de40edd8b7ac4673c2602ce95cbf40014b925
Kalle Hallden <kallehalldeen@gmail.com>
2021-08-18 11:58:58 +0200

    created login page animated line menu

--- app/exerlog/lib/UI/global.dart
+++ app/exerlog/lib/UI/global.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 
 // colors
 Color backgroundColor = Color(0xFF2D2B41);
+Color textColorLoginForm = Color(0xFFACACB4);
+Color textColorBlue = Color(0xFF31A6DC);
 
 // text styles
 
 TextStyle buttonText = TextStyle(color: backgroundColor, fontFamily: "Avenir", fontWeight: FontWeight.w800, fontSize: 18);
+TextStyle loginOptionTextStyle = TextStyle( letterSpacing: 1,color: textColorLoginForm, fontFamily: "Avenir", fontWeight: FontWeight.w300, fontSize: 18);
+TextStyle loginOptionTextStyleSelected = TextStyle( letterSpacing: 1,color: textColorBlue, fontFamily: "Avenir", fontWeight: FontWeight.w400, fontSize: 18);
