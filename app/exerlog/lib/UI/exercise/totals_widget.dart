ff099645a87166e9210babdc97c991bf5d3d0650
kallehallden <kallehalldeen@gmail.com>
2021-12-08 23:39:58 +0100

    updated the UI for my phone

--- app/exerlog/lib/UI/exercise/totals_widget.dart
+++ app/exerlog/lib/UI/exercise/totals_widget.dart
@@ -1,4 +1,3 @@
-
 import 'package:exerlog/UI/global.dart';
 import 'package:exerlog/UI/gradient_button.dart';
 import 'package:exerlog/UI/gradient_button.dart';
@@ -20,27 +19,28 @@ class _TotalsWidgetState extends State<TotalsWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-            height: 35,
-            width: 100,
-            child: RaisedGradientButton(
-              child: Text(widget.totals.total[widget.index], style: buttonTextSmall,), 
-              gradient: LinearGradient(
-                colors: <Color>[Color(0xFF34D1C2), Color(0xFF31A6DC)],
-              ), 
-              onPressed: () {
-                setState(() {
-                  widget.index += 1;
-                  if (widget.index == 4) {
-                    widget.index = 0;
-                  }
-                });
-              }, 
-              radius: 30, 
-              borderSize: 0
-              ),
-          );
+      height: 35,
+      width: 150,
+      child: RaisedGradientButton(
+          child: Text(
+            widget.totals.total[widget.index],
+            style: buttonTextSmall,
+          ),
+          gradient: LinearGradient(
+            colors: <Color>[Color(0xFF34D1C2), Color(0xFF31A6DC)],
+          ),
+          onPressed: () {
+            setState(() {
+              widget.index += 1;
+              if (widget.index == 4) {
+                widget.index = 0;
+              }
+            });
+          },
+          radius: 30,
+          borderSize: 0),
+    );
   }
-
 }
 
 class TotalsData {
