a9395aef00e0e49733677dc82f6d08c55cf809e9
Kalle Hallden <kallehalldeen@gmail.com>
2022-03-01 18:04:49 +0100

    changed design for totals widget for prev workout

--- app/exerlog/lib/UI/prev_workout/prev_workout_page.dart
+++ app/exerlog/lib/UI/prev_workout/prev_workout_page.dart
@@ -93,7 +93,6 @@ class _PrevWorkoutPageState extends State<PrevWorkoutPage> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Container(),
                     WorkoutTotalsWidget(
                       totals: workoutData.totals,
                     ),
