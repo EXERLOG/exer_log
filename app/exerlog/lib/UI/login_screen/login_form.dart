3f82b9cdf003bf9c76ae516316fbdcd9a6aa19c4
Jorge Luís <jr.luisrodrigues@gmail.com>
2022-08-05 09:22:34 -0300

    fix(auth): Auth pages responsiveness

--- app/exerlog/lib/UI/login_screen/login_form.dart
+++ app/exerlog/lib/UI/login_screen/login_form.dart
@@ -1,112 +1,108 @@
-
 import 'package:exerlog/UI/global.dart';
 import 'package:exerlog/UI/login_screen/google_signin_button.dart';
 import 'package:exerlog/UI/login_screen/login_data.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class LoginForm extends StatefulWidget {
-  LoginData loginData;
+  final LoginData loginData;
 
-  LoginForm(this.loginData);
+  const LoginForm(this.loginData);
+
   @override
   _LoginFormState createState() => _LoginFormState();
-
 }
 
 class _LoginFormState extends State<LoginForm> {
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    return Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-              Container(
-                height: height*0.22,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: greenTextColor,
+          ),
+          child: Container(
+            margin: EdgeInsets.all(1),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: backgroundColor,
+            ),
+            child: Container(
+              padding: EdgeInsets.only(left: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: 40,
+                    child: Icon(
+                      Icons.mail,
                       color: greenTextColor,
                     ),
-                    child: Container(
-                      margin: EdgeInsets.all(1),
-                      decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: backgroundColor,
-                    ),
-                      child: Container(
-                        padding: EdgeInsets.only(left: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Container(
-                              width: 40,
-                              child: Icon(Icons.mail, color: greenTextColor,)),
-                            Container(
-                              width: MediaQuery.of(context).size.width * 0.65,
-                              child: TextField(
-                                onChanged: (value) {
-                                  widget.loginData.email = value;
-                                },
-                                style: mediumTitleStyleWhite,
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: "email/username"
-                                )
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
-                    )
                   ),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: greenTextColor,
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.65,
+                    child: TextField(
+                      onChanged: (value) {
+                        widget.loginData.email = value;
+                      },
+                      style: mediumTitleStyleWhite,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "email/username",
+                      ),
                     ),
-                    child: Container(
-                      margin: EdgeInsets.all(1),
-                      decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: backgroundColor,
-                    ),
-                      child: Container(
-                        padding: EdgeInsets.only(left: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Container(
-                              width: 40,
-                              child: Icon(Icons.lock, color: greenTextColor,)),
-                            Container(
-                              width: MediaQuery.of(context).size.width * 0.65,
-                              child: TextField(
-                                onChanged: (value) {
-                                  widget.loginData.password = value;
-                                },
-                                obscureText: true,
-                                style: mediumTitleStyleWhite,
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: "password"
-                                )
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
-                    )
                   ),
-                ],),
+                ],
               ),
-              GoogleSignInButton()
-            ],)
-        );
+            ),
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: greenTextColor,
+          ),
+          child: Container(
+            margin: EdgeInsets.all(1),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: backgroundColor,
+            ),
+            child: Container(
+              padding: EdgeInsets.only(left: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 40,
+                    child: Icon(
+                      Icons.lock,
+                      color: greenTextColor,
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.65,
+                    child: TextField(
+                      onChanged: (value) {
+                        widget.loginData.password = value;
+                      },
+                      obscureText: true,
+                      style: mediumTitleStyleWhite,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "password",
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        GoogleSignInButton()
+      ],
+    );
   }
-
 }
--- app/exerlog/lib/UI/login_screen/login_page.dart
+++ app/exerlog/lib/UI/login_screen/login_page.dart
@@ -1,33 +1,29 @@
-import 'package:exerlog/Bloc/authentication.dart';
 import 'package:exerlog/Bloc/email_signup.dart';
-import 'package:exerlog/Bloc/user_bloc.dart';
-import 'package:exerlog/Bloc/workout_bloc.dart';
-import 'package:exerlog/Models/exercise.dart';
-import 'package:exerlog/Models/sets.dart';
-import 'package:exerlog/Models/user.dart';
-import 'package:exerlog/Models/workout.dart';
-import 'package:exerlog/UI/login_screen/google_signin_button.dart';
+import 'package:exerlog/UI/global.dart';
+import 'package:exerlog/UI/gradient_button.dart';
 import 'package:exerlog/UI/login_screen/login_data.dart';
 import 'package:exerlog/UI/login_screen/login_form.dart';
 import 'package:exerlog/UI/login_screen/signup_form.dart';
-import 'package:exerlog/UI/workout/workout_builder.dart';
 import 'package:exerlog/UI/workout/workout_page.dart';
 import 'package:exerlog/main.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import '../gradient_button.dart';
-import '../global.dart';
-import '../maxes/max_builder.dart';
 
 class LoginPage extends StatefulWidget {
-  LoginPage(this.title, {Key? key}) : super(key: key);
-  String title = "0";
+  final String title;
+
+  const LoginPage({
+    Key? key,
+    this.title = "0",
+  }) : super(key: key);
+
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-  LoginData loginData = new LoginData('', '');
+class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
+  late TabController controller;
+
+  LoginData loginData = LoginData('', '');
   bool login = true;
   int index = 0;
   List<Tab> tabs = <Tab>[
@@ -45,140 +41,127 @@ class _LoginPageState extends State<LoginPage> {
   ];
   int id = 0;
 
-  
+  @override
+  void initState() {
+    super.initState();
+
+    controller = TabController(length: tabs.length, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
+    final height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: backgroundColor,
-        body: Container(
-          padding: EdgeInsets.only(top: height*0.25, bottom: height*0.3, left: 30, right: 30),
+      backgroundColor: backgroundColor,
+      body: Container(
+        padding: EdgeInsets.only(
+          left: 30,
+          right: 30,
+        ),
+        child: Center(
           child: Container(
-            child: Stack(children: [
-              Container(
-                height: height*0.42,
-                decoration: BoxDecoration(
-                    color: Color(0xFF2E2C42),
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Color.fromRGBO(0, 0, 0, 0.2),
-                          offset: Offset(0, 3),
-                          blurRadius: 10,
-                          spreadRadius: 10)
-                    ]),
-                child: DefaultTabController(
-                    length: tabs.length,
-                    // The Builder widget is used to have a different BuildContext to access
-                    // closest DefaultTabController.
-                    child: Builder(builder: (BuildContext context) {
-                      final TabController tabController =
-                          DefaultTabController.of(context)!;
-                      tabController.addListener(() {
-                        if (!tabController.indexIsChanging) {
-                          setState(() {
-                            index = tabController.index;
-                            print(index);
-                          });
-                          // Your code goes here.
-                          // To get index of current tab use tabController.index
-                        }
-                      });
-                      return Column(
-                        children: [
-                          Container(
-                            height: 30,
-                            child: AppBar(
-                              backgroundColor: Colors.transparent,
-                              shadowColor: Colors.transparent,
-                              flexibleSpace: TabBar(
-                                tabs: tabs,
-                              ),
-                            ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.all(10),
-                            height: height*0.34,
-                            child: TabBarView(
-                              children: [
-                                LoginForm(loginData),
-                                SignupForm(loginData)
-                              ],
-                            ),
-                          ),
-                        ],
-                      );
-                    })),
-              ),
-              Positioned(
-                child: RaisedGradientButton(
-                  radius: 30,
-                  child: Text(index > 0 ? "Sign up" : "Login", style: buttonText),
-                  gradient: LinearGradient(
-                    colors: <Color>[Color(0xFF34D1C2), Color(0xFF31A6DC)],
+            height: height * 0.42,
+            decoration: BoxDecoration(
+              color: Color(0xFF2E2C42),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromRGBO(0, 0, 0, 0.2),
+                  offset: Offset(0, 3),
+                  blurRadius: 10,
+                  spreadRadius: 10,
+                )
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  height: 30,
+                  child: AppBar(
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    flexibleSpace: TabBar(tabs: tabs, controller: controller),
                   ),
-                  onPressed: () async {
-                    if (index == 0) {
-                      // login with email and password
-                      if (loginData.password != '' && loginData.email != '') {
-                        User? user = await EmailSignup.signInWithEmailAndPassword(loginData.email, loginData.password).then((value) {
-                          userID = value?.uid;
-                          return value;
-                        });
-                       
-                
-                if (user != null) {
-                  print("USER IS NOT NULL");
-                  userID = user.uid;
-                  the_user = user;
-                  setState(() {
-                    Navigator.pop(context);
-                    Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => WorkoutPage(null
-                      ),
-                    ),
-                  );
-                  });                 
-                }
-                      }
-                    }
-                    else {
-                      // signup with email and password
-                      if (loginData.password != '' && loginData.email != '') {
-                        User? user = await EmailSignup.registerWithEmail(loginData.email, loginData.password).then((value) {
-                          userID = value?.uid;
-                          return value;
-                        });
-                       
-                
-                if (user != null) {
-                  print("USER IS NOT NULL");
-                  userID = user.uid;
-                  the_user = user;
-                  setState(() {
-                    Navigator.pop(context);
-                    Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => WorkoutPage(null
-                      ),
-                    ),
-                  );
-                  });                 
-                }
-                      }
-                    }
-                  },
                 ),
-                right: 60,
-                left: 60,
-                bottom: 0,
-              )
-            ]),
+                const SizedBox(height: 16),
+                Expanded(
+                  child: TabBarView(
+                    controller: controller,
+                    children: [
+                      LoginForm(loginData),
+                      SignupForm(loginData),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(60, 0, 60, 16),
+                  child: RaisedGradientButton(
+                    radius: 30,
+                    child: Text(
+                      index > 0 ? "Sign up" : "Login",
+                      style: buttonText,
+                    ),
+                    gradient: LinearGradient(
+                      colors: <Color>[Color(0xFF34D1C2), Color(0xFF31A6DC)],
+                    ),
+                    onPressed: () async {
+                      if (index == 0) {
+                        // login with email and password
+                        if (loginData.password != '' && loginData.email != '') {
+                          final user = await EmailSignup.signInWithEmailAndPassword(
+                            loginData.email,
+                            loginData.password,
+                          );
+
+                          if (user != null) {
+                            print("USER IS NOT NULL");
+
+                            userID = user.uid;
+                            the_user = user;
+
+                            Navigator.pop(context);
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => WorkoutPage(null),
+                              ),
+                            );
+                          }
+                        }
+                      } else {
+                        // signup with email and password
+                        if (loginData.password != '' && loginData.email != '') {
+                          final user = await EmailSignup.registerWithEmail(
+                            loginData.email,
+                            loginData.password,
+                          );
+
+                          if (user != null) {
+                            print("USER IS NOT NULL");
+
+                            userID = user.uid;
+                            the_user = user;
+
+                            Navigator.pop(context);
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => WorkoutPage(null),
+                              ),
+                            );
+                          }
+                        }
+                      }
+                    },
+                  ),
+                )
+              ],
+            ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
 
