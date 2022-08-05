bd60919d985f3d63450ec2a8df8fd6a3d8afe8f4
Momshad Dinury <dinurymomshad.dev@gmail.com>
2022-08-06 00:05:18 +0600

    Added logout button, refactored gradient_button, refactored calendar_page

--- app/exerlog/lib/UI/calendar/calendar_page.dart
+++ app/exerlog/lib/UI/calendar/calendar_page.dart
@@ -1,7 +1,10 @@
 import 'package:exerlog/UI/global.dart';
 import 'package:exerlog/UI/gradient_button.dart';
 import 'package:exerlog/UI/workout/workout_page.dart';
+import 'package:exerlog/main.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
 import 'calendar_widget.dart';
 
 class CalendarPage extends StatefulWidget {
@@ -16,40 +19,81 @@ class _CalendarPageState extends State<CalendarPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
-      body: Container(
-        padding: EdgeInsets.only(top:30, left: 20, right: 20),
-        color: backgroundColor,
+      appBar: AppBar(
+        title: Text("Exerlog"),
+        backgroundColor: backgroundColor.withOpacity(0.75),
+        actions: [
+          _Logout(),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             CalendarWidget(),
             Container(
-              width: MediaQuery.of(context).size.width *0.8,
-              margin: EdgeInsets.only(bottom: 30),
+              width: MediaQuery.of(context).size.width * 0.8,
               child: RaisedGradientButton(
-                gradient: LinearGradient(
-                  colors: <Color>[Color(0xFF34D1C2), Color(0xFF31A6DC)],
-                ),
-                radius: 30,
                 child: Text(
                   "START NEW WORKOUT",
                   style: buttonTextSmall,
                 ),
                 onPressed: () {
-                  Navigator.of(context).pop();
-                  Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => WorkoutPage(null
-                        ),
-                      ),
-                    );
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => WorkoutPage(null),
+                    ),
+                  );
                 },
               ),
             )
           ],
-          ),
+        ),
       ),
     );
   }
+}
 
+class _Logout extends StatefulWidget {
+  const _Logout({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<_Logout> createState() => _LogoutState();
+}
+
+class _LogoutState extends State<_Logout> {
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 16.0),
+      child: InkWell(
+        onTap: () async {
+          if (!_isLoading) {
+            setState(() {
+              _isLoading = true;
+            });
+            await FirebaseAuth.instance.signOut();
+            setState(() {
+              _isLoading = false;
+            });
+            Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (context) => MyApp()),
+              (route) => false,
+            );
+          }
+        },
+        child: _isLoading
+            ? Center(
+                child: CircularProgressIndicator(strokeWidth: 2),
+              )
+            : Icon(Icons.logout),
+      ),
+    );
+  }
 }
--- app/exerlog/lib/UI/gradient_button.dart
+++ app/exerlog/lib/UI/gradient_button.dart
@@ -1,51 +1,54 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class RaisedGradientButton extends StatefulWidget {
-  Widget child;
-  final Gradient gradient;
-  final double width;
-  final double height;
-  final Function onPressed;
-  final double radius;
   RaisedGradientButton({
+    required this.onPressed,
     required this.child,
-    required this.gradient,
     this.width = double.infinity,
     this.height = 50.0,
-    required this.onPressed,
-    required this.radius,
+    this.radius = 30,
+    this.gradient,
   });
+
+  final Function onPressed;
+  final Widget child;
+  final double width;
+  final double height;
+  final double radius;
+  final Gradient? gradient;
+
   @override
   _RaisedGradientButtonState createState() => _RaisedGradientButtonState();
-
 }
 
 class _RaisedGradientButtonState extends State<RaisedGradientButton> {
   @override
   Widget build(BuildContext context) {
-    return 
-      DecoratedBox(
-        decoration: BoxDecoration(
-            gradient: widget.gradient,
-            borderRadius: BorderRadius.circular(widget.radius)
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        gradient: widget.gradient ??
+            LinearGradient(
+              colors: <Color>[
+                Color(0xFF34D1C2),
+                Color(0xFF31A6DC),
+              ],
+            ),
+        borderRadius: BorderRadius.circular(widget.radius),
+      ),
+      child: TextButton(
+        onPressed: () {
+          widget.onPressed();
+        },
+        child: widget.child,
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(widget.radius),
+            ),
           ),
-        child: TextButton(
-          onPressed: () {widget.onPressed();}, 
-          child: widget.child,
-          style: ButtonStyle(
-            //backgroundColor: MaterialStateProperty.all(Colors.transparent),
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(widget.radius),
-                //side: BorderSide(width: widget.borderSize)
-                
-              )
-            )
-          ),
-          ),
-      );
+        ),
+      ),
+    );
   }
-
 }
