86fcaa0eefd418e501a84292e8100b9f9b5e4c2b
Momshad Dinury <dinurymomshad.dev@gmail.com>
2022-08-12 22:42:45 +0600

    refactor: text field added, widgets moved

---
+++ app/exerlog/lib/src/feature/authentication/widgets/login_form.dart
@@ -0,0 +1,35 @@
+import 'package:exerlog/src/feature/authentication/controller/authentication_controller.dart';
+import 'package:exerlog/src/feature/authentication/widgets/google_signin_button.dart';
+import 'package:exerlog/src/utils/validators.dart';
+import 'package:flutter/material.dart';
+
+import 'text_field.dart';
+
+class LoginForm extends StatelessWidget {
+  const LoginForm(this.controller);
+
+  final AuthenticationController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        OutlinedTextField(
+          hinText: "Email",
+          leadingIcon: Icons.email,
+          validator: (value) => Validators.requiredField(value),
+          textEditingController: controller.emailTextEditingController,
+        ),
+        OutlinedTextField(
+          hinText: "Password",
+          leadingIcon: Icons.lock,
+          validator: (value) => Validators.requiredField(value),
+          textEditingController: controller.passwordTextEditingController,
+          obscureText: true,
+        ),
+        GoogleSignInButton()
+      ],
+    );
+  }
+}
