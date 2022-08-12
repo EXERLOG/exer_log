6cab4aba9c150ca467759299fd85f239d34b7c85
Momshad Dinury <dinurymomshad.dev@gmail.com>
2022-08-12 17:26:30 +0600

    feat: Dependency added to access any dependencies via provider

---
+++ app/exerlog/lib/src/dependency.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+abstract class Dependency {
+  static Provider<FirebaseAuth> get firebaseAuth => _firebaseProvider;
+  static Provider<GoogleSignIn?> get googleSignIn => _googleSignInProvider;
+}
+
+final _firebaseProvider = Provider<FirebaseAuth>((ref) {
+  return FirebaseAuth.instance;
+});
+
+final _googleSignInProvider = Provider<GoogleSignIn?>((ref) {
+  return GoogleSignIn();
+});
