dd1f624d2d010407e9da580715ade4ca5b6f9a8e
Momshad Dinury <dinurymomshad.dev@gmail.com>
2022-08-12 22:03:16 +0600

    chore: added the text field editing controllers in authentication controller

--- app/exerlog/lib/src/feature/authentication/controller/authentication_controller.dart
+++ app/exerlog/lib/src/feature/authentication/controller/authentication_controller.dart
@@ -1,17 +1,19 @@
+import 'package:exerlog/main.dart';
 import 'package:exerlog/src/core/base/base_state.dart';
-import 'package:exerlog/src/core/base/shared_preference/shared_pref_key.dart';
-import 'package:exerlog/src/core/base/shared_preference/shared_pref_wrapper.dart';
+import 'package:exerlog/src/core/base/shared_preference/shared_preference_b.dart';
 import 'package:exerlog/src/dependency.dart';
 import 'package:exerlog/src/feature/authentication/data/authentication_repository.dart';
 import 'package:exerlog/src/utils/logger/logger.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final authStateProvider = StreamProvider<User?>(
   (ref) => ref.watch(Dependency.firebaseAuth).authStateChanges(),
 );
 
-final authenticationProvider = StateNotifierProvider(
+final _authenticationProvider =
+    StateNotifierProvider.autoDispose<AuthenticationController, BaseState>(
   (ref) => AuthenticationController(ref: ref),
 );
 
@@ -24,23 +26,34 @@ class AuthenticationController extends StateNotifier<BaseState> {
   late AuthenticationRepository _repository;
   late User user;
 
-  static StateNotifierProvider<AuthenticationController, dynamic>
-      get controller => authenticationProvider;
+  static AutoDisposeStateNotifierProvider<AuthenticationController, BaseState>
+      get provider => _authenticationProvider;
 
-  Future<void> signIn({required String email, password}) async {
+  static AutoDisposeProviderBase<AuthenticationController> get controller =>
+      _authenticationProvider.notifier;
+
+  Future<void> signIn() async {
     try {
       state = LoadingState();
-      await _repository.signIn(email: email, password: password);
+      await _repository.signIn(
+        email: emailTextEditingController.text,
+        password: passwordTextEditingController.text,
+      );
       authStateChangeStatus();
     } on FirebaseAuthException catch (e) {
+      Log.error(e.code);
+      Log.error(e.message);
       state = ErrorState(message: e.message);
     }
   }
 
-  Future<void> signUp({required String email, password}) async {
+  Future<void> signUp() async {
     try {
       state = LoadingState();
-      await _repository.signUp(email: email, password: password);
+      await _repository.signUp(
+        email: emailTextEditingController.text,
+        password: passwordTextEditingController.text,
+      );
       authStateChangeStatus();
     } on FirebaseAuthException catch (e) {
       Log.error(e.code);
@@ -72,7 +85,11 @@ class AuthenticationController extends StateNotifier<BaseState> {
           this.user = user;
           try {
             setValue(USER_UID, this.user.uid);
-            print(this.user.uid);
+            Log.info(this.user.uid);
+
+            /// TODO: Remove later
+            userID = user.uid;
+            the_user = user;
           } catch (e, stackTrace) {
             Log.error(e.toString());
             Log.error(stackTrace.toString());
@@ -87,4 +104,23 @@ class AuthenticationController extends StateNotifier<BaseState> {
   }
 
   Future<void> signOut() async => await _repository.signOut();
+
+  /// Text Editing Controllers
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+  TextEditingController confirmPasswordTextEditingController = TextEditingController();
+
+  void clearTextFields() {
+    emailTextEditingController.clear();
+    passwordTextEditingController.clear();
+    confirmPasswordTextEditingController.clear();
+  }
+
+  bool isSamePassword() => passwordTextEditingController.text == confirmPasswordTextEditingController.text;
+
+  @override
+  void dispose() {
+    clearTextFields();
+    super.dispose();
+  }
 }
