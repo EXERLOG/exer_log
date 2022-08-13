28112cf62f240e9c285c85058988feb59f0d4a50
Momshad Dinury <dinurymomshad.dev@gmail.com>
2022-08-13 22:31:04 +0600

    chore: updated global.dart

--- lib/UI/global.dart
+++ lib/UI/global.dart
@@ -17,36 +17,41 @@ TextStyle whiteTextStyleSmall = TextStyle(
   fontSize: 12,
 );
 TextStyle buttonTextSmall = TextStyle(
-    color: backgroundColor,
-    fontFamily: "Avenir",
-    fontWeight: FontWeight.w800,
-    fontSize: 12);
+  color: backgroundColor,
+  fontFamily: "Avenir",
+  fontWeight: FontWeight.w800,
+  fontSize: 12,
+);
 TextStyle buttonText = TextStyle(
-    color: backgroundColor,
-    fontFamily: "Avenir",
-    fontWeight: FontWeight.w800,
-    fontSize: 18);
+  color: backgroundColor,
+  fontFamily: "Avenir",
+  fontWeight: FontWeight.w800,
+  fontSize: 18,
+);
 TextStyle loginOptionTextStyle = TextStyle(
-    letterSpacing: 1,
-    color: textColorLoginForm,
-    fontFamily: "Avenir",
-    fontWeight: FontWeight.w300,
-    fontSize: 18);
+  letterSpacing: 1,
+  color: textColorLoginForm,
+  fontFamily: "Avenir",
+  fontWeight: FontWeight.w300,
+  fontSize: 18,
+);
 TextStyle loginOptionTextStyleSelected = TextStyle(
-    letterSpacing: 1,
-    color: textColorBlue,
-    fontFamily: "Avenir",
-    fontWeight: FontWeight.w400,
-    fontSize: 18);
+  letterSpacing: 1,
+  color: textColorBlue,
+  fontFamily: "Avenir",
+  fontWeight: FontWeight.w400,
+  fontSize: 18,
+);
 TextStyle mediumTitleStyleWhite = TextStyle(
-    letterSpacing: 1,
-    color: Colors.white,
-    fontFamily: "Avenir",
-    fontWeight: FontWeight.bold,
-    fontSize: 18);
+  letterSpacing: 1,
+  color: Colors.white,
+  fontFamily: "Avenir",
+  fontWeight: FontWeight.bold,
+  fontSize: 18,
+);
 TextStyle setHintStyle = TextStyle(
   letterSpacing: 1,
-  color: Colors.white.withOpacity(1),
+  color: Colors.white.withOpacity(0.2),
   fontFamily: "Avenir",
   fontSize: 15,
 );
@@ -64,11 +69,12 @@ TextStyle greenButtonTextThin = TextStyle(
   fontSize: 18,
 );
 TextStyle smallTitleStyleWhite = TextStyle(
-    letterSpacing: 1,
-    color: Colors.white,
-    fontFamily: "Avenir",
-    fontWeight: FontWeight.bold,
-    fontSize: 12);
+  letterSpacing: 1,
+  color: Colors.white,
+  fontFamily: "Avenir",
+  fontWeight: FontWeight.bold,
+  fontSize: 12,
+);
 
 // one rep max calculation table
 List<double> maxTable = [
