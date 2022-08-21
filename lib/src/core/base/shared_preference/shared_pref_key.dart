f04fe06b791e06b182eef50335c0ed766d3a4302
Momshad Dinury <dinurymomshad.dev@gmail.com>
2022-08-21 13:00:44 +0600

    re-patch: #86 & #40

--- lib/src/core/base/shared_preference/shared_pref_key.dart
+++ lib/src/core/base/shared_preference/shared_pref_key.dart
@@ -1,1 +1,2 @@
 const String USER_UID = 'USER_UID';
+const String IS_LOGGED_IN = '';
--- lib/src/feature/onboarding/splash/view/splash_screen.dart
+++ lib/src/feature/onboarding/splash/view/splash_screen.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 
-import 'package:exerlog/UI/global.dart';
-import 'package:exerlog/src/feature/authentication/view/authentication_wrapper.dart';
+import 'package:exerlog/UI/calendar/view/calendar_page.dart';
+import 'package:exerlog/main.dart';
+import 'package:exerlog/src/feature/authentication/view/landing_screen.dart';
 import 'package:exerlog/src/utils/assets.dart';
 import 'package:exerlog/src/widgets/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:exerlog/src/core/base/shared_preference/shared_preference_b.dart';
 
 class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -19,7 +22,13 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
   void initState() {
     super.initState();
     Timer(Duration(milliseconds: 2000), () {
-      if (mounted) _navigateToAuthWrapper();
+      if (SharedPref.getBoolAsync(IS_LOGGED_IN, defaultValue: false)) {
+        /// TODO: Remove later
+        userID = SharedPref.getStringAsync(USER_UID);
+        if (mounted) _navigateToCalendarScreen();
+      } else {
+        if (mounted) _navigateToLandingScreen();
+      }
     });
   }
 
@@ -46,11 +55,19 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
     );
   }
 
-  void _navigateToAuthWrapper() {
+  void _navigateToLandingScreen() {
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(
-        builder: (context) => AuthenticationWrapper(),
+        builder: (context) => LandingScreen(),
+      ),
+    );
+  }
+
+  void _navigateToCalendarScreen() {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (context) => CalendarPage(),
       ),
     );
   }
