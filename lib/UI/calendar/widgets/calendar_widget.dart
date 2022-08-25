d454fb52a2a8403296c0a477949d021fc4333173
Momshad Dinury <dinurymomshad.dev@gmail.com>
2022-08-25 23:11:46 +0600

    refactor: calendar widget refactored

--- lib/UI/calendar/widgets/calendar_widget.dart
+++ lib/UI/calendar/widgets/calendar_widget.dart
@@ -3,13 +3,8 @@ import 'package:exerlog/UI/global.dart';
 import 'package:exerlog/src/widgets/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
 
-class CalendarWidget extends StatefulWidget {
-  @override
-  _CalendarWidgetState createState() => _CalendarWidgetState();
-}
-
-class _CalendarWidgetState extends State<CalendarWidget> {
-  List<String> monthNames = [
+class CalendarWidget extends StatelessWidget {
+  final List<String> monthNames = [
     "January",
     "February",
     "March",
@@ -26,11 +21,9 @@ class _CalendarWidgetState extends State<CalendarWidget> {
 
   @override
   Widget build(BuildContext context) {
-    screenHeight = MediaQuery.of(context).size.height;
-    screenWidth = MediaQuery.of(context).size.width;
     return ThemeProvider(builder: (context, theme) {
       return Container(
-        padding: EdgeInsets.all(10),
+        padding: EdgeInsets.all(20),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
           boxShadow: [
@@ -43,35 +36,33 @@ class _CalendarWidgetState extends State<CalendarWidget> {
           ],
           color: theme.colorTheme.backgroundColorVariation,
         ),
-        height: screenHeight * 0.4,
         child: Column(
           children: [
-            Container(
-              height: screenHeight * 0.05,
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10.0),
               child: Text(
-                monthNames[DateTime.now().month - 1],
+                _getCurrentMonth(),
                 style: mediumTitleStyleWhite,
               ),
             ),
-            Container(
-              margin: EdgeInsets.only(left: 20, right: 20),
-              child: Divider(
-                color: Colors.white.withOpacity(0.5),
-              ),
+            Divider(
+              color: Colors.white.withOpacity(0.75),
+              thickness: 1,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: getDates(),
+              children: _getDates(),
             )
           ],
         ),
       );
     });
   }
+
+  String _getCurrentMonth() => monthNames[DateTime.now().month - 1];
 }
 
-List<Widget> getDates() {
+List<Widget> _getDates() {
   List weekdayNames = [
     'MON',
     'TUE',
@@ -81,59 +72,67 @@ List<Widget> getDates() {
     'SAT',
     'SUN',
   ];
-  List<Widget> datelist = [];
+  List<Widget> dates = [];
   int today = DateTime.now().day;
   int month = DateTime.now().month;
   int year = DateTime.now().year;
-  int weekday = DateTime.now().weekday;
   DateTime now = DateTime.now();
 
-  List week_list = [[], [], [], [], [], [], []];
-  List<Widget> dates = [];
-  // month could span 4-6 weeks which is 4-6 columns
-  // which day of the week does the first fall on
+  List weekList = [[], [], [], [], [], [], []];
+
+  /// Month could span 4-6 weeks which is 4-6 columns
+  /// which day of the week does the first fall on
   DateTime first = now.subtract(Duration(days: today - 1));
   DateTime last = new DateTime(year, month + 1, 0);
-  // if the first of the month falls anywhere else than on a monday
-  // then get the dates before the first as well
 
-  // calculate how many days and weeks should be shown in the calendar
-  DateTime prev_month = first.subtract(Duration(days: first.weekday));
+  /// If the first of the month falls anywhere else than on a monday
+  /// then get the dates before the first as well
 
-  int days_to_show = last.day + (first.weekday - 1) + (7 - last.weekday);
-  double weeks = days_to_show / 7;
+  /// Calculate how many days and weeks should be shown in the calendar
+  DateTime previousMonth = first.subtract(Duration(days: first.weekday));
+
+  int daysToShow = last.day + (first.weekday - 1) + (7 - last.weekday);
+  double weeks = daysToShow / 7;
   if (weeks is! int) {
-    // if the weeks is not an int then we need to add an extra week
+    /// If the weeks is not an int then we need to add an extra week
     weeks = weeks.toInt() + 1;
   }
 
-  int current_day = 0;
+  int currentDay = 0;
   int j = 0;
-  while (current_day != days_to_show) {
+  while (currentDay != daysToShow) {
     if (j == 7) {
       j = 0;
     }
 
-    DateTime day_to_add = prev_month.add(Duration(days: current_day + 1));
-    week_list[j].add(day_to_add);
+    DateTime dayToAdd = previousMonth.add(Duration(days: currentDay + 1));
+    weekList[j].add(dayToAdd);
     j++;
-    current_day++;
+    currentDay++;
   }
 
   for (int i = 0; i < 7; i++) {
-    List<Widget> dates = [];
-    dates.add(Text(
-      weekdayNames[i],
-      style: setStyle,
-    ));
-    for (int j = 0; j < week_list[i].length; j++) {
-      dates.add(DateWidget(week_list[i][j]));
+    List<Widget> _dates = [];
+
+    /// Add all weekdays name
+    _dates.add(
+      Text(
+        weekdayNames[i],
+        style: setStyle,
+      ),
+    );
+    for (int j = 0; j < weekList[i].length; j++) {
+      _dates.add(
+        DateWidget(weekList[i][j]),
+      );
     }
-    datelist.add(Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: dates,
-    ));
+    dates.add(
+      Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: _dates,
+      ),
+    );
   }
 
-  return datelist;
+  return dates;
 }
