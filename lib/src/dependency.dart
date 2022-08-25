6dcfda616896c1e159f78f227952fb80c792abff
Momshad Dinury <dinurymomshad.dev@gmail.com>
2022-08-25 22:43:26 +0600

    chore: connectivity dependency added

--- lib/src/dependency.dart
+++ lib/src/dependency.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity/connectivity.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -6,6 +7,9 @@ abstract class Dependency {
   static Provider<FirebaseAuth> get firebaseAuth => _firebaseAuthProvider;
 
   static Provider<GoogleSignIn?> get googleSignIn => _googleSignInProvider;
+
+  static StreamProvider<ConnectivityResult> get connectivityResult =>
+      _onConnectivityChangedStreamProvider;
 }
 
 final _firebaseAuthProvider = Provider<FirebaseAuth>((ref) {
@@ -14,4 +18,14 @@ final _firebaseAuthProvider = Provider<FirebaseAuth>((ref) {
 
 final _googleSignInProvider = Provider<GoogleSignIn?>((ref) {
   return GoogleSignIn();
+});
+
+/// Connectivity
+final _connectivityProvider = Provider<Connectivity>((ref) {
+  return Connectivity();
+});
+
+final _onConnectivityChangedStreamProvider =
+    StreamProvider<ConnectivityResult>((ref) {
+  return ref.read(_connectivityProvider).onConnectivityChanged;
 });
