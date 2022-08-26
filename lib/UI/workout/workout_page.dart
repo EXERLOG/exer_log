d05f7dad9c9a6359744160f2ed893fb30dbbf7a8
Momshad Dinury <dinurymomshad.dev@gmail.com>
2022-08-26 21:25:53 +0600

    chore: removed unused imports

--- lib/UI/workout/workout_page.dart
+++ lib/UI/workout/workout_page.dart
@@ -10,7 +10,6 @@ import 'package:exerlog/UI/workout/save_workout_dialog.dart';
 import 'package:exerlog/UI/workout/workout_name_selection_widget.dart';
 import 'package:exerlog/UI/workout/workout_toatals_widget.dart';
 import 'package:exerlog/src/core/theme/app_theme.dart';
-import 'package:exerlog/src/feature/calendar/view/calendar_screen.dart';
 import 'package:exerlog/src/widgets/gradient_button.dart';
 import 'package:exerlog/src/widgets/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
