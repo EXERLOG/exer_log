f711c9e9613c1f7404088dd9d33ba136bfa2f9e2
Krzysztof Hoffmann <krzysztof.hoffmann@increase.pl>
2022-09-16 22:10:05 +0200

    feat: extend ThemeData in MaterialApp (#153)
    * Feat: extend ThemeData in MaterialApp
    
    * Feat: extend ThemeData in MaterialApp
    
    Co-authored-by: Angel Aviel Domaoan <13580338+tenshiAMD@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:exerlog/src/core/theme/theme_color.dart';
 import 'package:exerlog/src/feature/onboarding/splash/view/splash_screen.dart';
 import 'package:exerlog/src/utils/logger/logger.dart';
 import 'package:exerlog/src/utils/logger/riverpod_logger.dart';
@@ -44,6 +45,21 @@ class MyApp extends ConsumerWidget {
     return MaterialApp(
       title: 'EXERLOG',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          snackBarTheme: SnackBarThemeData(
+              backgroundColor: ThemeColor.red,
+              contentTextStyle: TextStyle(color: ThemeColor.secondary)),
+          scaffoldBackgroundColor: ThemeColor.darkBlueVariation,
+          dialogBackgroundColor: ThemeColor.darkBlueVariation,
+          appBarTheme: AppBarTheme(
+              backgroundColor: ThemeColor.darkBlue.withOpacity(0.75)),
+          colorScheme: ColorScheme.light().copyWith(
+            primary: ThemeColor.primary,
+            secondary: ThemeColor.secondary,
+            error: ThemeColor.red,
+            background: ThemeColor.darkBlueVariation,
+          )),
+      darkTheme: ThemeData(),
       home: SplashScreen(),
     );
   }
